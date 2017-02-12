2c5dbb53ebef2cb4dbb471d30b598aac73961fd2
Darren Ross <matach@gmail.com>
2017-02-11 23:04:55 -0800

    Initial commit. Set up of textbook project, solution, and classes for each chapter of the textbook.

---
+++ textbook/MakingDecisions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textbook
+{
+    class MakingDecisions
+    {
+        public static void main()
+        {
+            Console.WriteLine("Welcome to the Chapter on Making Decisions.");
+            Console.WriteLine("Enter the exercise name to begin an exercise program.");
+            Console.WriteLine("Please enter one of the following to ");
+            Console.WriteLine("Number Guessing");
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            switch (input)
+            {
+                case "number guessing":
+                    NumberGuess();
+                    break;
+                default:
+                    Console.WriteLine("This is not a valid selection. Please try again.");
+                   break;
+            }
+        }
+
+        public static void NumberGuess()
+        {
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            int min = 1;
+            int max = 10;
+            int guess;
+            randomNumber = ranNumberGenerator.Next(min, max);
+            Console.WriteLine("There is only one rule: Guess a number between 1 and 10.");
+            guess = Convert.ToInt16(Console.ReadLine());
+
+            if (guess < randomNumber)
+                Console.WriteLine("Your guess is too low.");
+            else if (guess == randomNumber)
+                Console.WriteLine("You guessed correctly.");
+            else if (guess > randomNumber)
+                Console.WriteLine("Your guess is too high.");
+            else
+                Console.WriteLine("Your guess is not a legal number in this game.");
+        }
+    }
+}
---
+++ textbook/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textbook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my textbook exercise program!");
+            Console.WriteLine("This program is broken down chapter by chapter and by exercise.");
+            Console.WriteLine("Please select a chapter to go to the chapter page. The chapter page will have a list of available exercises.");
+            Console.WriteLine("Making decisions");
+            Console.WriteLine("Looping");
+            Console.WriteLine("Using Arrays");
+            Console.WriteLine("Using Methods");
+            Console.WriteLine("Advanced Methods");
+            Console.WriteLine("Using Classes and Objects");
+            Console.WriteLine("Inheritance");
+            Console.WriteLine("Exception Handling");
+            Console.WriteLine("Using Controls");
+            Console.WriteLine("Handling Events");
+            Console.WriteLine("Files and Streams");
+            Console.WriteLine("LINQ");
+            Console.WriteLine("To select a chapter please enter the chapter topic.");
+
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            switch (input)
+            {
+                case "making decisions":
+                    MakingDecisions.main();
+                    break;
+                case "looping":
+                    Looping.main();
+                    break;
+                case "using arrays":
+                    Arrays.main();
+                    break;
+                case "using methods":
+                    Methods.main();
+                    break;
+                case "advanced methods":
+                    AdvancedMethods.main();
+                    break;
+                case "using classes and objects":
+                    ClassesObjects.main();
+                    break;
+                case "inheritance":
+                    Inheritance.main();
+                    break;
+                case "exception handling":
+                    ExceptionHandling.main();
+                    break;
+                case "using controls":
+                    Controls.main();
+                    break;
+                case "handling events":
+                    HandlingEvents.main();
+                    break;
+                case "files and streams":
+                    FilesStreams.main();
+                case "linq":
+                    Linq.main();
+                    break;
+                default:
+                    Console.WriteLine("This is not a valid selection. Please try again.");
+                    break;
+            }
+        }
+    }
+}
