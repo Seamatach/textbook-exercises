5cc22c7495485238a54ccd1a0ae97b0f9b52832d
Darren Ross <matach@gmail.com>
2018-12-21 01:33:43 -0800

    Added new Method exercises.

--- textbook/Methods.cs
+++ textbook/Methods.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +16,9 @@ namespace textbook
             Console.WriteLine("Enter the exercise name to begin an exercise program.");
             Console.WriteLine("Please enter one of the following to:");
             Console.WriteLine("Generate Memo");
-            //Console.WriteLine("Check Lower");
-            //Console.WriteLine("Average Calc");
-            //Console.WriteLine("Yard sale");
+            Console.WriteLine("Siding Estimator");
+            Console.WriteLine("Convert inches to cm");
+            Console.WriteLine("Yard sale");
 
             string input = Console.ReadLine();
             input = input.ToLower();
@@ -25,20 +27,20 @@ namespace textbook
                 case "generate memo":
                     GenerateMemo();
                     break;
-                //    case "check lower":
-                //        CheckLower();
-                //        break;
-                //    case "average calc":
-                //        AverageCalc();
-                //        break;
-                //    case "yard sale":
-                //        YardSale();
-                //        break;
-                //    default:
-                //        Console.WriteLine("This is not a valid selection. Please try again.");
-                //        break;
+                case "siding estimator":
+                    SidingEstimate();
+                    break;
+                case "convert inches to cm":
+                    ConvertInchesToCm();
+                    break;
+                    //    case "yard sale":
+                    //        YardSale();
+                    //        break;
+                    //    default:
+                    //        Console.WriteLine("This is not a valid selection. Please try again.");
+                    //        break;
 
-                // Need to add an exit option here and at all menus in all classes.
+                    // Need to add an exit option here and at all menus in all classes.
             }
         }
 
@@ -66,6 +68,7 @@ namespace textbook
                     Console.Write(" has the best chance of survival from here on out.");
                 }
 
+                Console.WriteLine("Do you want to create another memo?");
                 ConsoleKeyInfo cki = Console.ReadKey(true);
                 keepPlaying = cki.KeyChar == 'y';
             }
@@ -76,40 +79,68 @@ namespace textbook
             Console.Write("C# Software Developers");
         }
 
-        //    public static void Method2()
-        //    {
-        //        bool keepPlaying = true;
-        //        while (keepPlaying)
-        //        {
+        public static void SidingEstimate()
+        {
+            bool keepPlaying = true;
+            while (keepPlaying)
+            {
+                int estimated;
+                Console.WriteLine("What is the length of the house?");
+                int lengthHouse = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the depth of your house?");
+                int depthHouse = Convert.ToInt32(Console.ReadLine());
+
+                estimated = Estimator(lengthHouse, depthHouse);
+                Console.WriteLine($"Your estimate is {estimated}.");
+
+                Console.WriteLine("Do you have another estimate");
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                keepPlaying = cki.KeyChar == 'y';
+            }
+        }
+
+        public static int Estimator(int length, int depth)
+        {
+            int cost = 5;
+            int area = length * depth;
+            int subTotalCost = cost * area;
+            
+            return subTotalCost;
+        }
+
+        public static void ConvertInchesToCm()
+        {
+            bool keepPlaying = true;
+            while (keepPlaying)
+            {
+                Console.WriteLine("How many inches to convert?");
+                double inches = Convert.ToInt32(Console.ReadLine());
+
+                double newLength = Convertor(inches);
+                Console.WriteLine($"{inches} is converted to: {newLength} centimeters.");
+                Console.WriteLine("Do you want to do another inch conversion?");
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                keepPlaying = cki.KeyChar == 'y';
+            }
+        }
+
+        public static double Convertor(double inch)
+        {
+            double cm = 2.54;
+            double converted = inch * cm;
+            return converted;
+        }
+
+        public static void YardSale()
+        {
+            bool keepPlaying = true;
+            while (keepPlaying)
+            {
 
 
-        //            ConsoleKeyInfo cki = Console.ReadKey(true);
-        //            keepPlaying = cki.KeyChar == 'y';
-        //        }
-        //    }
-
-        //    public static void Method3()
-        //    {
-        //        bool keepPlaying = true;
-        //        while (keepPlaying)
-        //        {
-
-
-        //            ConsoleKeyInfo cki = Console.ReadKey(true);
-        //            keepPlaying = cki.KeyChar == 'y';
-        //        }
-        //    }
-
-        //    public static void Method4()
-        //    {
-        //        bool keepPlaying = true;
-        //        while (keepPlaying)
-        //        {
-
-
-        //            ConsoleKeyInfo cki = Console.ReadKey(true);
-        //            keepPlaying = cki.KeyChar == 'y';
-        //        }
-        //    }
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                keepPlaying = cki.KeyChar == 'y';
+            }
+        }
     }
 }
